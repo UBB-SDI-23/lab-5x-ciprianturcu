6605fc6e9a078bdf79f2f04b1a4ab690f48ab36c
Turcu Ciprian <ciprianstelian_08@yahoo.com>
2023-04-07 19:43:05 +0300

    trying to make it work

--- react-frontend/src/constants.ts
+++ react-frontend/src/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "http://ec2-13-50-237-1.eu-north-1.compute.amazonaws.com/api";
+const PROD_BACKEND_API_URL = "http://ec2-13-49-148-32.eu-north-1.compute.amazonaws.com/api";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
 
 export const BACKEND_API_URL =
