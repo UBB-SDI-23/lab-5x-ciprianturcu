93607fb4502e47b95a33c84d024d0bff4649e7df
Turcu Ciprian <ciprianstelian_08@yahoo.com>
2023-04-09 17:24:48 +0300

    .

--- react-frontend/src/components/client/ClientUpdate.tsx
+++ react-frontend/src/components/client/ClientUpdate.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, IconButton, InputLabel, Select, TextField } from "@mui/material";
+import { Button, Card, CardActions, CardContent, IconButton, InputLabel, Select, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -98,12 +98,13 @@ export const ClientUpdate = () => {
 							value={client.type}
 							onChange={(event) => setClient({...client, type: event.target.value as ClientType})}
 						>
-							{Object.values(ClientType).map((typeVal) => (
-								<option key={typeVal} value={typeVal}>
-									{typeVal}
+							{Object.keys(ClientType).map((key) => (
+								<option key={key} value={key}>
+									{key}
 								</option>
 							))}
 						</Select>
+						<Button type="submit">Update Tennis Player</Button>
 					</form>
 				</CardContent>
 				<CardActions>
