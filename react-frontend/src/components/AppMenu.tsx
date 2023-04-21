2dc84dd7c314b670083b3e15fc4c41b47b400479
Turcu Ciprian <ciprianstelian_08@yahoo.com>
2023-04-21 22:56:54 +0300

    added routes to lawsuit ui

--- react-frontend/src/components/AppMenu.tsx
+++ react-frontend/src/components/AppMenu.tsx
@@ -45,6 +45,16 @@ export const AppMenu = () => {
 					</Button>
 
 					<Button
+						variant={path.startsWith("/lawsuit") ? "outlined" : "text"}
+						to="/lawsuit"
+						component={Link}
+						color="inherit"
+						sx={{ mr: 5 }}
+						startIcon={<LocalLibraryIcon />}>
+						Lawsuits
+					</Button>
+
+					<Button
 						variant={path.startsWith("/top-profits") ? "outlined" : "text"}
 						to="/top-profits"
 						component={Link}
