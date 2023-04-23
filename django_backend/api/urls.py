4731aba961a15735c3be707f72ca098785ba5bbf
Turcu Ciprian <ciprianstelian_08@yahoo.com>
2023-04-23 11:49:25 +0300

    .

--- django_backend/api/urls.py
+++ django_backend/api/urls.py
@@ -6,16 +6,16 @@ from api.views.ClientViews import ClientList, ClientDetail, LawsuitsOfClientList
 from api.views.LawsuitViews import LawsuitList, LawsuitDetail, ProfitStatisticView
 
 urlpatterns = [
-    path('attorney/', AttorneyList.as_view()),
+    path('attorney/', AttorneyList.as_view(), name="attorney"),
     path('attorney/<int:pk>/', AttorneyDetail.as_view()),
-    path('lawsuit/', LawsuitList.as_view()),
+    path('lawsuit/', LawsuitList.as_view(), name="lawsuit"),
     path('lawsuit/<int:pk>/', LawsuitDetail.as_view()),
-    path('client/', ClientList.as_view()),
+    path('client/', ClientList.as_view(), name="client"),
     path('client/<int:pk>/', ClientDetail.as_view()),
-    path('aol/', AttorneyOnLawsuitList.as_view()),
+    path('aol/', AttorneyOnLawsuitList.as_view(), name="aol"),
     path('aol/<int:pk>/', AttorneyOnLawsuitDetail.as_view()),
     path('profits/', ProfitStatisticView.as_view()),
-    path('travel_sheet/', AttorneyTravelView.as_view()),
+    path('travel_sheet/', AttorneyTravelView.as_view(), name="travel_sheet"),
     path('client/<int:id>/lawsuit/', LawsuitsOfClientList.as_view()),
     path('client/autocomplete/', ClientViewForAutocomplete.as_view())
 ]
